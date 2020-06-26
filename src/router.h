b56815e0be12c6d5b2e1499e2e39e31be3eb85df
doctry <jonathanyang475869@gmail.com>
2020-06-27 00:28:46 +0800

    router partial work

--- src/router.h
+++ src/router.h
@@ -1,55 +1,89 @@
-#include <stdlib.h>
-#include <fstream>
-#include <iostream>
-#include "die.h"
+/***********************************************************************
 
-#ifndef ROUTER
-#define ROUTER
+  FileName    [router.h]
+
+  Author      [Yang Chien Yi]
+
+  This file difines the router set. A_star.
+
+***********************************************************************/
+
+#pragma once
+
+////////////////////////////////////////////////////////////////////////
+///                           INCLUDES                               ///
+////////////////////////////////////////////////////////////////////////
+
+#include <queue>
+#include <utility>
+#include <vector>
+#include "grid.h"
+#include "PrimeMan.h"
+
+////////////////////////////////////////////////////////////////////////
+///                          DESCRIPTION                             ///
+////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////
+///                           CLASSES                                ///
+////////////////////////////////////////////////////////////////////////
+
+enum cost_type {
+    Manhattan,
+
+};
+
+class MyComp {
+   public:
+    MyComp() {}
+    bool operator()(const Grid* lhs, const Grid* rhs) const {
+        return ((lhs->get_estimated()) > (rhs->get_estimated()));
+    }
+};
+
+// typedef
+typedef std::vector<grid*> GridList;
+typedef std::vector<unsigned> IdxList;
+typedef std::priority_queue<Grid*, std::vector<Grid*>, MyComp> priority_grid;
+
+// initialization
 
 class Router {
    public:
-    Router(std::fstream& inputfile, std::fstream& outputfile)
-        : _outputfile(outputfile), _err(0), _cost(0) {
-        // read inputfile
-        int x, y, z;
-        inputfile >> x >> y >> z;
-        unsigned x1, y1, z1, x2, y2, z2;
-        _die = new Die(x, y, z, Manhattan);
-        while (inputfile >> x1 >> y1 >> z1 >> x2 >> y2 >> z2) {
-            unsigned idx1 = _die->get_idx(x1, y1, z1);
-            unsigned idx2 = _die->get_idx(x2, y2, z2);
-            _NetList.push_back(idx1);
-            if (!_die->add_net(idx1, idx2)) {
-                std::cerr << "invalid coordinate: " << x1 << " " << y1 << " "
-                          << z1 << " " << x2 << " " << y2 << " " << z2
-                          << std::endl;
-                _err = true;
-            }
-        }
-        _die->add_normal();
-        inputfile.close();
-    }
-    void rout() {
-        for (int i = 0; i < _NetList.size(); i++) {
-            IdxList ans;
-            _cost += _die->A_star(_NetList[i], ans);
-            for (int j = 0; j < ans.size(); j++) {
-                _outputfile << _die->get_x(ans[j]) << " " << _die->get_y(ans[j])
-                            << " " << _die->get_z(ans[j]) << std::endl;
-            }
-            _outputfile << std::endl;
-        }
-        std::cout << "total cost: " << _cost << std::endl;
-        _outputfile.close();
-    }
-    bool err() const { return _err; }
+    // friend class of cost functions
+    friend class CostGen;
+    friend class ManDist;
+    friend priority_grid;
+
+    // public functions
+    Router(PrimeMan& pm);
+    ~Router();
+    int A_star(const unsigned);
+
+    // cost functions
+    int get_cost(unsigned a, unsigned b);
+    int ManDist(unsigned a, unsigned b) const;
 
    private:
-    Die* _die;
-    IdxList _NetList;
-    std::fstream& _outputfile;
-    bool _err;
-    int _cost;
-};
+    PrimeMan& _pm;
+    GridList _GridList;
+    priority_grid* _PriorityGrid;
+    cost_type _CostType;
+    // constant
+    const unsigned _area;
+    //
 
-#endif
+    // private functions
+    bool propagate(unsigned, unsigned);
+    bool sub_propagate(int, unsigned, unsigned, unsigned);
+    void init_cost(cost_type);
+    void backtrace(unsigned, IdxList&);
+    void reset();
+    void use_grid(unsigned);
+    void clear_space(const unsigned);
+    void revive(const unsigned);
+    void revive_grid(const unsigned);
+    void init_clear(const unsigned, const unsigned);
+    void init_grid(const unsigned, const unsigned);
+    // operator overloading
+};
